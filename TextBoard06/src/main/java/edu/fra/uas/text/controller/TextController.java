b3589b68eda124fe5cb32b27d0f07eb727b4c3b7
nielssei <107681969+nielssei@users.noreply.github.com>
2023-03-02 17:38:41 +0100

    Update TextController.java

--- TextBoard06/src/main/java/edu/fra/uas/text/controller/TextController.java
+++ TextBoard06/src/main/java/edu/fra/uas/text/controller/TextController.java
@@ -7,23 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import edu.fra.uas.text.model.Text;
-import edu.fra.uas.text.service.TextService;
+import edu.fra.uas.text.serve
+
+// Controller handlet Requests vom Client
 
 @Controller
 public class TextController {
 	
+	
 	@Autowired
 	private TextService textService;
 	
-	@GetMapping("/texts")
-	public String listTexts(Model model) {
+	// URL Mapping für alle Texte und Rückgabe der HTML Datei (Handling von GET Requests)
+ 	@GetMapping("/texts")
+	public Strirng listTexts(Model model) {
 		
 		model.addAttribute("texts", textService.getAllTexts());
 		return "texts";
 	}
-	
+	// Handling für "Neuer Text" Button
 	@GetMapping("/texts/new")
 	public String createTextForm(Model model) {
 		
@@ -32,20 +35,20 @@ public class TextController {
 		return "create_text";
 		
 	}
-	
+	// Post Request Handling für Speicherfunktion neuer Texte
 	@PostMapping("/texts")
 	public String saveText(@ModelAttribute("text") Text text) {
 		textService.saveText(text);
 		return "redirect:/texts";
 		
 	}
-	
+	// Get Handling für das Zeigen einzelner Texte, um diese zu bearbeiten / anzusehen
 	@GetMapping("/texts/edit/{id}")
 	public String editTextForm(@PathVariable Long id, Model model) {
 		model.addAttribute("text", textService.getTextById(id));
 		return "edit_text";	
 	}
-
+	// Post Handling für Speichern bearbeiteter Texte
 	@PostMapping("/texts/{id}")
 	public String updateStudent(@PathVariable Long id,
 				@ModelAttribute("text") Text text, Model model) {
@@ -58,7 +61,7 @@ public class TextController {
 		textService.updateText(existingText);
 		return "redirect:/texts";
 	}
-	
+	// Get Handling für das Löschen einzelner Texte
 	@GetMapping("texts/{id}")
 	public String deleteStudent(@PathVariable Long id) {
 		textService.deleteTextById(id);
