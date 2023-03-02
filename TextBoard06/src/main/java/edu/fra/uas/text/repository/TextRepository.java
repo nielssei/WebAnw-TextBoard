baa5778d59a42d6e02c74a89dfe6565e69725720
nielssei <107681969+nielssei@users.noreply.github.com>
2023-03-02 17:43:49 +0100

    Update TextRepository.java

--- TextBoard06/src/main/java/edu/fra/uas/text/repository/TextRepository.java
+++ TextBoard06/src/main/java/edu/fra/uas/text/repository/TextRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import edu.fra.uas.text.model.Text;
 
+// Repository, um alle Texte in einer Datenbank zu speichern wird mithilfe von JPARepository realisiert
 @Repository
 public interface TextRepository extends JpaRepository<Text, Long> {
     List<Text> findByTitle(String title);
