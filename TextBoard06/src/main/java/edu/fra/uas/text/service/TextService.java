62e2e1bccbed9bcdc0fff211f3ce0f3527bbe7b5
nielssei <107681969+nielssei@users.noreply.github.com>
2023-03-02 17:54:13 +0100

    Update TextService.java

--- TextBoard06/src/main/java/edu/fra/uas/text/service/TextService.java
+++ TextBoard06/src/main/java/edu/fra/uas/text/service/TextService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import edu.fra.uas.text.model.Text;
 
+// Interface zum Zugriff auf Service Methoden durch Controller. Methoden werden implementiert in TextServiceImpl.
 public interface TextService {
 		
 	List<Text> getAllTexts();
